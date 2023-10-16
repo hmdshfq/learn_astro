9a7e932cb891b188501b2c8ae74767cae35cc8a8
pavel <pavel.polyakov0010@gmail.com>
2023-10-16 12:55:06 +0200

    change astro.config.mjs for work with netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
 import { defineConfig } from 'astro/config';
 import preact from "@astrojs/preact";
 
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
 });
