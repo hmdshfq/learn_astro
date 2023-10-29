63277e2ca2a18331770a50345fe8fa2c8b032cac
pavel <pavel.polyakov0010@gmail.com>
2023-10-29 18:14:08 +0100

    Delete usless files, Clean up code.

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,4 +1,3 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
-    document.querySelector('.mobile-nav-menu').classList.toggle('expanded');
+    document.querySelector('.mobile-menu').classList.toggle('expanded');
 });
