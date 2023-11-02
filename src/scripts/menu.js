3cf5faa677f1756137adae63b99f986ed976e8df
pavel <pavel.polyakov0010@gmail.com>
2023-11-02 23:03:20 +0100

    Close mobile menu when clicked outside. Add resume downlad button. Add icons (links) for skills.

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,24 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.mobile-menu').classList.toggle('expanded');
-});
+})
+
+// Hide menu if clicked out of the menu
+window.addEventListener('click', function (e) {
+    if (!document.getElementById('mobile-nav-menu').contains(e.target)
+        && !document.getElementById('menu').contains(e.target)) {
+        document.querySelector('.mobile-menu').classList.toggle('expanded');
+    }
+})
+
+// Hide menu if clicked on the link
+// menuButtonElement.addEventListener('click', () => {
+//     mobileMenuElement.classList.toggle('expanded');
+// })
+
+
+//For multiple page. Show the hamburge menu if you go to another page.
+// document.addEventListener('astro:page-load', () => {
+//     document.querySelector('.hamburger').addEventListener('click', () => {
+//         document.querySelector('.mobile-menu').classList.toggle('expanded');
+//     })
+// });
