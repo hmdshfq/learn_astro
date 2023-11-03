4af8f7d4b3a0cc49afbd1d1c29928eeae3dab749
pavel <pavel.polyakov0010@gmail.com>
2023-11-03 01:55:39 +0100

    Add to menu.js function that closed movbile menu when clicked on the link. Clear trash from other files.

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -4,21 +4,27 @@ document.querySelector('.hamburger').addEventListener('click', () => {
 
 // Hide menu if clicked out of the menu
 window.addEventListener('click', function (e) {
-    if (!document.getElementById('mobile-nav-menu').contains(e.target)
-        && !document.getElementById('menu').contains(e.target)) {
-        document.querySelector('.mobile-menu').classList.toggle('expanded');
-    }
+    if (document.querySelector('.mobile-menu').classList.contains('expanded')
+        &&
+        !document.getElementById('mobile-nav-menu').contains(e.target)
+        && 
+        !document.getElementById('menu').contains(e.target)) {
+            document.querySelector('.mobile-menu').classList.toggle('expanded');
+        }
 })
 
 // Hide menu if clicked on the link
-// menuButtonElement.addEventListener('click', () => {
-//     mobileMenuElement.classList.toggle('expanded');
-// })
+let mobileMenuElements = document.querySelectorAll('.menu-button');
+
+for (var i = 0; i < mobileMenuElements.length; i++){
+    mobileMenuElements[i].addEventListener('click', () => {
+            document.querySelector('.mobile-menu').classList.toggle('expanded');
+        })
+}
 
 
-//For multiple page. Show the hamburge menu if you go to another page.
-// document.addEventListener('astro:page-load', () => {
-//     document.querySelector('.hamburger').addEventListener('click', () => {
+// mobileMenuElements.map( (element) => {
+//     element.addEventListener('click', () => {
 //         document.querySelector('.mobile-menu').classList.toggle('expanded');
 //     })
-// });
+// })
