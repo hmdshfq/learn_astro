73eefeee8e5f5d59fe2e6cc4156ba02e28c50c48
pavel <pavel.polyakov0010@gmail.com>
2023-11-16 21:38:33 +0100

    remake jump animation by Intersection Observer API delete waypoint/inview libs

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,23 +1,57 @@
-$(document).ready(function () {
+//Observer API
+let jumpElements;
 
-    var inview = new Waypoint.Inview({
-        element: $('.jump-when-visible'),
-        enter: function (direction) {
-            if(direction === "up") {
-                $('.jump-when-visible').addClass('jump-animation-up');
-            } else {
-                $('.jump-when-visible').addClass('jump-animation-down');
+window.addEventListener(
+    "load",
+    (event) => {
+
+        jumpElements = document.querySelectorAll('.jump-when-visible');
+        console.log(jumpElements);
+
+        createJumpObserver();
+    },
+    false,
+);
+
+function createJumpObserver() {
+
+    let options = {
+        root: null, // use the document's viewport as the container
+        rootMargin: "0px",  // % or px - offsets added to each side of the intersection 
+        threshold: 0.7,    // percentage of the target element which is visible
+    };
+
+    // Create the intersection observer instance by calling its constructor and passing it a
+    // callback function to be run whenever a threshold is crossed in one direction or the other:
+    let observer = new IntersectionObserver(callback, options);
+
+    // Get all the `.jump-when-visible` from DOM and attach the observer to these
+    jumpElements.forEach(element => { observer.observe(element) });
+}
+
+let callback = (entries) => {
+    entries.forEach(entry => {
+        const ratio = entry.intersectionRatio;
+        const boundingRect = entry.boundingClientRect;
+        const intersectionRect = entry.intersectionRect;
+
+        // If entry is visible - according with the params set in `options`
+        // then, depends on what direction, adds `jump-animation-up/down` class to box
+        // otherwise removes `jump-animation-up/down` class
+
+        if (entry.isIntersecting) {
+            if (ratio < 1) {
+                if (boundingRect.top < intersectionRect.top) {
+                    entry.target.classList.add('jump-animation-up');
+                    console.log("on the top");
+                } else {
+                    entry.target.classList.add('jump-animation-down');
+                    console.log("on the bottom");
+                }
             }
-            // console.log("enter function direction: " + direction);
-        },
-        entered: function (direction) {
-        },
-        exit: function (direction) {
-            $('.jump-when-visible').removeClass('jump-animation-up');
-            $('.jump-when-visible').removeClass('jump-animation-down');
-        },
-        exited: function (direction) {
+        } else {
+            entry.target.classList.remove('jump-animation-up');
+            entry.target.classList.remove('jump-animation-down');
         }
-    });
-
-});
+    })
+}
