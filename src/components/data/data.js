9c5e965d98a1de0a65dcf01a3bad4836a10309a6
pavel <pavel.polyakov0010@gmail.com>
2023-12-15 18:19:02 +0100

    Fill new project add _blank to the project's links

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -63,8 +63,8 @@ export const certificateData = [
 
 //ProjectCard.astro
 export const projectData = [
-    {imageSrc: "/src/assets/images/project/Product_Landing_page.png", alt: "Project product landing page", name: "Product Landing Page", description: "Simple page with pure HTML and CSS", websiteLink: "https://pavelpaliakou.github.io/product_landing_page/", githubLink: "https://github.com/PavelPaliakou/product_landing_page"},
-    {imageSrc: "/src/assets/images/project/Product_Landing_page.png", alt: "Project product landing page", name: "Product Landing Page", description: "Simple page with pure HTML and CSS", websiteLink: "https://pavelpaliakou.github.io/product_landing_page/", githubLink: "https://github.com/PavelPaliakou/product_landing_page"},
+    {imageSrc: "/src/assets/images/project/Product_Landing_page.png", alt: "Product landing page", name: "Product Landing Page", description: "Simple page with pure HTML and CSS", websiteLink: "https://pavelpaliakou.github.io/product_landing_page/", githubLink: "https://github.com/PavelPaliakou/product_landing_page"},
+    {imageSrc: "/src/assets/images/project/Weather_page.png", alt: "Weather page", name: "Weather page", description: "HTML, TailWindCSS and JS with OpenWeather API", websiteLink: "https://pavelpaliakou.github.io/weather_page/", githubLink: "https://github.com/PavelPaliakou/weather_page"},
     {imageSrc: "/src/assets/images/project/Product_Landing_page.png", alt: "Project product landing page", name: "Product Landing Page", description: "Simple page with pure HTML and CSS", websiteLink: "https://pavelpaliakou.github.io/product_landing_page/", githubLink: "https://github.com/PavelPaliakou/product_landing_page"}
 ];
 
